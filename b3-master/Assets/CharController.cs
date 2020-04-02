2f0b978bde9b72c7879a8eaf55ab72dfff83e956
vr266 <vr266@scarletmail.rutgers.edu>
2020-04-02 10:13:12 -0400

    Changes to WASD Moves
    Ditto

---
+++ b3-master/Assets/CharController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    public Animator aperson;
+
+    // Start is called before the first frame update
+    void Start(){
+        aperson=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            aperson.SetFloat("Horizontal",-1);
+        }
+        else{
+            aperson.SetFloat("Horizontal",1);
+        }
+    }
+}
